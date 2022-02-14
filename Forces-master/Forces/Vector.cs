2b2c079c26f576f0978b44d3b9462fca781d7a84
JohnyBoardProfi <91531366+JohnyBoardProfi@users.noreply.github.com>
2022-02-14 20:23:55 +0200

    Add files via upload

---
+++ Forces-master/Forces/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Forces
+{
+    public class Vector
+    {
+        public Vector() : this(0, 0) { }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector Projection(Vector onVector) => this * onVector / onVector.SquareAbs * onVector;
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.X + b.X, a.Y + b.Y);
+
+        public static Vector operator -(Vector v) => new Vector(-v.X, -v.Y);
+
+        public static Vector operator -(Vector a, Vector b) => a + -b;
+
+        public static Vector operator *(Vector v, double n) => new Vector(v.X * n, v.Y * n);
+
+        public static Vector operator *(double n, Vector v) => v * n;
+
+        public static Vector operator /(Vector v, double n) => v * (1 / n);
+
+        public static double operator *(Vector a, Vector b) => a.X * b.X + a.Y * b.Y;
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double SquareAbs => this * this;
+
+        public double Abs => Math.Round(Math.Sqrt(SquareAbs));
+    }
+}
